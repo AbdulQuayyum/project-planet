88988b7f94e379b0d9409ac67f32597943489f0c
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-01-31 20:38:41 +0100

    Cooking

--- Index.js
+++ Index.js
@@ -1,7 +1,13 @@
 const { parse } = require("csv-parse")
 const fs = require("fs")
 
-const results = []
+const HabitablePlanetLists = []
+
+function HabitablePlanet(Planet) {
+    return Planet["koi_disposition"] === "CONFIRMED"
+        && Planet["koi_insol"] > 0.36 && Planet["koi_insol"] < 1.11
+        && Planet["koi_prad"] < 1.6
+}
 
 fs.createReadStream("./Data/KeplerData.csv")
     .pipe(parse({
@@ -9,13 +15,17 @@ fs.createReadStream("./Data/KeplerData.csv")
         columns: true,
     }))
     .on("data", (data) => {
-        results.push(data)
+        if (HabitablePlanet(data)) {
+            HabitablePlanetLists.push(data)
+        }
     })
     .on("error", (err) => {
         console.log(err)
     })
     .on("end", () => {
-        console.log(results)
-        console.log("Done")
+        console.log(HabitablePlanetLists.map((Planet) => {
+            return Planet["kepler_name"]
+        }))
+        console.log(`${HabitablePlanetLists.length} possible habitable planets were found according to the Kepler data provided`)
     })
 // parse()
